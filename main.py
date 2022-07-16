356b033820ce967079a9bfaf3b9f4445473a3c0c
krischal111 <krischal111@gmail.com>
2022-07-17 00:26:40 +0545

    working routine code + json file

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import os
+from tracemalloc import start
+
+from pyparsing import delimited_list
 import discord
 import json
 from datetime import datetime
@@ -12,10 +15,70 @@ def Log(msg):
 async def on_ready():
   Log(f'Logged in as {client.user}')
 
-def routine_print():
+def routine_format(today_routine, times) -> str:
+  Holiday='Holiday'
+  # print(type(Holiday))
+  if (today_routine == Holiday) :
+    return str(
+    '''
+    ********************
+    Today is the holiday
+      Enjoy your day!!
+    ********************
+    '''
+    )
+
+  else:
+    routine_str = ""
+    sameperiod = False
+    period_str =""
+    for i in range(len(today_routine)):
+        start_time = times[i]
+        end_time   = times[i+1]
+        time_str = f"{start_time:5} - {end_time:5} : "
+        period = today_routine[i]
+
+        if period == "Same":
+          routine_str += time_str+period_str
+          continue
+        else:
+          period_str = ""
+
+        if period in ["Break", ""]:
+          routine_str += time_str + period + '\n'
+        else:
+          subject, type, teachers = period
+          if type == 'L':
+            period_str += "Lec  of"
+          elif type == 'P':
+            period_str += "Prac of"
+          elif type == 'T':
+            period_str += "Tut  of"
+          
+          period_str += f" {subject:17} by {str(teachers)}\n"
+          routine_str +=  time_str+period_str
+
+
+
+
+    return routine_str
+
+
+def routine_print(day) -> str:
   with open('default_routine.json') as routine_file:
-    routine = json.load(routine_file)
-    print(routine)
+    routine_json = json.load(routine_file)
+    routine = routine_json['routine']
+    times    = routine_json['times']
+    today = (datetime.now().weekday()+1)%7
+    days = routine_json["days"]
+
+    if day in days:
+      today = days.index(day)
+
+    today_routine = routine[today]
+    # print(routine_format(today_routine,times))
+    return routine_format(today_routine, times)
+    
   
 
 @client.event
@@ -54,9 +117,11 @@ async def on_message(message):
         bot_reply += '\n' + u.display_name
 
     elif words[0] in ['!routine','!sch','!schedule']:
-      routine_print()
-        
-      bot_reply = f"I am printing today's routine"
+      # routine_print()
+      arg = ''
+      if len(words) > 1:
+        arg = words[1]
+      bot_reply = f"```\n{routine_print(arg)}\n```"
 
 
       
@@ -67,8 +132,8 @@ async def on_message(message):
     if i_will_reply :
       await message.channel.send(bot_reply)
 
-# routine_print()
-client.run('OTg2MjgzODY5NzM1MTE2OTAy.Gpyc0S.ocNlu3aACeRZ_2r5c_x19-Xi0wlPUdaLoSYE7I')
+# print(routine_print(''))
+client.run(os.getenv('routiney_token'))
 
 
 
